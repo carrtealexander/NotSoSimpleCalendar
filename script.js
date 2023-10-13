b449513449df356d70356be847c2c08a36562eca
carrtealexander <143460245+carrtealexander@users.noreply.github.com>
2023-10-13 11:21:34 -0500

    Forgot Script

---
+++ script.js
@@ -0,0 +1,343 @@
+
+
+var today = dayjs();
+$('#currentDay').text(today.format('dddd,MMMM D YYYY'));
+
+
+
+var eventA = $( '#textarea0' )
+var eventB = $( '#textarea1' )
+var eventC = $( '#textarea2' )
+var eventD = $( '#textarea3' )
+var eventE = $( '#textarea4' )
+var eventF = $( '#textarea5' )
+var eventG = $( '#textarea6' )
+var eventH = $( '#textarea7' )
+var eventI = $( '#textarea8' )
+var eventJ = $( '#textarea9' )
+var eventK = $( '#textarea10')
+var eventL = $( '#textarea11')
+var eventM = $( '#textarea12')
+var eventN = $( '#textarea13')
+var eventO = $( '#textarea14')
+var eventP = $( '#textarea15')
+var eventQ = $( '#textarea16')
+var eventR = $( '#textarea17')
+var eventS = $( '#textarea18')
+var eventT = $( '#textarea19')
+var eventU = $( '#textarea20')
+var eventV = $( '#textarea21')
+var eventW = $( '#textarea22')
+var eventX = $( '#textarea23')
+
+
+var saveEventA = localStorage.getItem('eventA')
+eventA.val(localStorage.getItem('eventA'))
+var save0 = $( '#save0' )
+console.log(eventA)
+
+$( '#save0' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventA.val()
+
+    localStorage.setItem('eventA', inputvalue)
+});
+
+var saveEventB = localStorage.getItem('eventB')
+eventB.val(localStorage.getItem('eventB'))
+var save1 = $( '#save1' )
+console.log(eventB)
+
+$( '#save1' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventB.val()
+
+    localStorage.setItem('eventB', inputvalue)
+});
+var saveEventC = localStorage.getItem('eventC')
+eventC.val(localStorage.getItem('eventC'))
+var save2 = $( '#save2' )
+console.log(eventC)
+
+$( '#save2' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventC.val()
+
+    localStorage.setItem('eventC', inputvalue)
+});
+
+var saveEventD = localStorage.getItem('eventD')
+eventD.val(localStorage.getItem('eventD'))
+var save3 = $( '#save3' )
+console.log(eventD)
+
+$( '#save3' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventD.val()
+
+    localStorage.setItem('eventD', inputvalue)
+});
+var saveEventE = localStorage.getItem('eventE')
+eventE.val(localStorage.getItem('eventE'))
+var save4 = $( '#save4' )
+console.log(eventE)
+
+$( '#save4' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventE.val()
+
+    localStorage.setItem('eventE', inputvalue)
+});
+var saveEventF = localStorage.getItem('eventF')
+eventF.val(localStorage.getItem('eventF'))
+var save5 = $( '#save5' )
+console.log(eventF)
+
+$( '#save5' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventF.val()
+
+    localStorage.setItem('eventF', inputvalue)
+});
+
+
+
+var saveEventG = localStorage.getItem('eventG')
+eventG.val(localStorage.getItem('eventG'))
+var save6 = $( '#save6' )
+console.log(eventG)
+
+$( '#save6' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventG.val()
+
+    localStorage.setItem('eventG', inputvalue)
+});
+
+
+var saveEventH = localStorage.getItem('eventH')
+eventH.val(localStorage.getItem('eventH'))
+var save7 = $( '#save7' )
+console.log(eventH)
+
+$( '#save7' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventH.val()
+
+    localStorage.setItem('eventH', inputvalue)
+});
+
+var saveEventI = localStorage.getItem('eventI')
+eventI.val(localStorage.getItem('eventI'))
+var save8 = $( '#save8' )
+console.log(eventI)
+
+$( '#save8' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventI.val()
+
+    localStorage.setItem('eventI', inputvalue)
+});
+
+var saveEventJ = localStorage.getItem('eventJ')
+eventJ.val(localStorage.getItem('eventJ'))
+var save9 = $( '#save9' )
+console.log(eventJ)
+
+$( '#save9' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventJ.val()
+
+    localStorage.setItem('eventJ', inputvalue)
+});
+
+
+var saveEventK = localStorage.getItem('eventK')
+eventK.val(localStorage.getItem('eventK'))
+var save10 = $( '#save10' )
+console.log(eventK)
+
+$( '#save10' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventK.val()
+
+    localStorage.setItem('eventK', inputvalue)
+});
+
+
+var saveEventL = localStorage.getItem('eventL')
+eventL.val(localStorage.getItem('eventL'))
+var save11 = $( '#save11' )
+console.log(eventL)
+
+$( '#save11' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventL.val()
+
+    localStorage.setItem('eventL', inputvalue)
+});
+
+
+var saveEventM = localStorage.getItem('eventM')
+eventM.val(localStorage.getItem('eventM'))
+var save12 = $( '#save12' )
+console.log(eventM)
+
+$( '#save12' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventM.val()
+
+    localStorage.setItem('eventM', inputvalue)
+});
+
+var saveEventN = localStorage.getItem('eventN')
+eventN.val(localStorage.getItem('eventN'))
+var save113 = $( '#save13' )
+console.log(eventN)
+
+$( '#save13' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventN.val()
+
+    localStorage.setItem('eventN', inputvalue)
+});
+
+var saveEventO = localStorage.getItem('eventO')
+eventO.val(localStorage.getItem('eventO'))
+var save14 = $( '#save14' )
+console.log(eventO)
+
+$( '#save14' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventO.val()
+
+    localStorage.setItem('eventO', inputvalue)
+});
+
+var saveEventP = localStorage.getItem('eventP')
+eventP.val(localStorage.getItem('eventP'))
+var save15 = $( '#save15' )
+console.log(eventP)
+
+$( '#save15' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventP.val()
+
+    localStorage.setItem('eventP', inputvalue)
+});
+
+var saveEventQ = localStorage.getItem('eventQ')
+eventQ.val(localStorage.getItem('eventQ'))
+var save16 = $( '#save16' )
+console.log(eventQ)
+
+$( '#save16' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventQ.val()
+
+    localStorage.setItem('eventQ', inputvalue)
+});
+var saveEventR = localStorage.getItem('eventR')
+eventR.val(localStorage.getItem('eventR'))
+var save17 = $( '#save17' )
+console.log(eventR)
+
+$( '#save17' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventR.val()
+
+    localStorage.setItem('eventR', inputvalue)
+});
+
+var saveEventS = localStorage.getItem('eventS')
+eventS.val(localStorage.getItem('eventS'))
+var save18 = $( '#save18' )
+console.log(eventS)
+
+$( '#save18' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventS.val()
+
+    localStorage.setItem('eventS', inputvalue)
+});
+
+var saveEventT = localStorage.getItem('eventT')
+eventT.val(localStorage.getItem('eventT'))
+var save19 = $( '#save19' )
+console.log(eventT)
+
+$( '#save19' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventT.val()
+
+    localStorage.setItem('eventT', inputvalue)
+});
+
+var saveEventU = localStorage.getItem('eventU')
+eventU.val(localStorage.getItem('eventU'))
+var save20 = $( '#save20' )
+console.log(eventU)
+
+$( '#save20' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventU.val()
+
+    localStorage.setItem('eventU', inputvalue)
+});
+var saveEventV = localStorage.getItem('eventV')
+eventV.val(localStorage.getItem('eventV'))
+var save2 = $( '#save21' )
+console.log(eventV)
+
+$( '#save21' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventV.val()
+
+    localStorage.setItem('eventV', inputvalue)
+});
+var saveEventW = localStorage.getItem('eventW')
+eventW.val(localStorage.getItem('eventW'))
+var save22 = $( '#save22' )
+console.log(eventW)
+
+$( '#save22' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventW.val()
+
+    localStorage.setItem('eventW', inputvalue)
+});
+var saveEventX = localStorage.getItem('eventX')
+eventX.val(localStorage.getItem('eventX'))
+var save23 = $( '#save23' )
+console.log(eventX)
+
+$( '#save23' ).click( function() {
+    console.log(localStorage)
+    
+    var inputvalue = eventX.val()
+
+    localStorage.setItem('eventX', inputvalue)
+});
